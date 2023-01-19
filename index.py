e242fcc6015bee80d171894d94ba6a73566b0976
Peter Klingelhofer <peterklingelhofer@gmail.com>
2023-01-18 22:28:39 -0600

    new lines

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plt
 
+
 def scrapeTextFromWebPage(url):
     response = requests.get(url)
 
@@ -10,20 +11,24 @@ def scrapeTextFromWebPage(url):
 
     return parseHtmlContent.get_text()
 
+
 def getTextFromFile(file_path):
     with open(file_path, "r") as file:
         text = file.read()
 
     return text
 
+
 def generateWordCloud(text):
     stopwords = set(STOPWORDS)
 
-    wordcloud = WordCloud(stopwords = stopwords, background_color = "white").generate(text)
+    wordcloud = WordCloud(stopwords=stopwords,
+                          background_color="white").generate(text)
 
-    plt.imshow(wordcloud, interpolation = "bilinear")
+    plt.imshow(wordcloud, interpolation="bilinear")
     plt.axis("off")
     plt.show()
+
 
 webPage = int(input('Enter web page URL:'))
 
