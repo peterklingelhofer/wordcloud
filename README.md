75d6952acb0360ed177dd4ac5b5c183afc16442c
Peter Klingelhofer <peterklingelhofer@gmail.com>
2023-01-18 22:16:49 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Word Cloud
+Minimal command-line prompt interface for Open AI's text-davinci-003 model
+
+## Setup
+Retrieve an Open AI API key [here](https://beta.openai.com/account/api-keys) if you haven't gotten one yet.
+
+```sh
+git clone https://github.com/peterklingelhofer/wordcloud.git
+cd wordcloud
+python --version
+pip install requests
+pip install bs4
+pip install matplotlib
+```
+
+## Usage
+```sh
+python index.py
+```
---
+++ index.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.pyplot as plt
+
+def scrapeTextFromWebPage(url):
+    response = requests.get(url)
+
+    parseHtmlContent = BeautifulSoup(response.content, "html.parser")
+
+    return parseHtmlContent.get_text()
+
+def getTextFromFile(file_path):
+    with open(file_path, "r") as file:
+        text = file.read()
+
+    return text
+
+def generateWordCloud(text):
+    stopwords = set(STOPWORDS)
+
+    wordcloud = WordCloud(stopwords = stopwords, background_color = "white").generate(text)
+
+    plt.imshow(wordcloud, interpolation = "bilinear")
+    plt.axis("off")
+    plt.show()
+
+webPage = int(input('Enter web page URL:'))
+
+text = scrapeTextFromWebPage(webPage)
+
+generateWordCloud(text)
